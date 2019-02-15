8b88bdb4d6cee75033379b2481e652803c1bde23
asmodat <asmodat@gmail.com>
2019-02-15 23:54:32 +0100

    up

--- egs/egs_ref.py
+++ egs/egs_ref.py
@@ -381,8 +381,8 @@ class AllTxContainer():
                     if error_retry_count != 0 and error_retry_count % 10 == 0:
                         console.info("Pending transaction filter missing, re-establishing filter (" + str(error_retry_count) + "), processing block: (" + str(self.process_block) + ")...")
                         time.sleep(5)
-                    if len(self.new_tx_list_tmp) > 1000:
-                        console.info("Got more then 1000 pending tx'es (" + str(len(self.new_tx_list_tmp)) + "), skipping...")
+                    if len(self.new_tx_list_tmp) > 2500:
+                        console.info("Got more then 2500 pending tx'es (" + str(len(self.new_tx_list_tmp)) + "), skipping...")
                         break
                     try:
                         #self.new_tx_list_tmp = self.pending_filter.get_all_entries() 
